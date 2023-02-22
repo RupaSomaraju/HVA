e3553f814e0729906c6f52ccedbf8fdd070adb22
RupaSomaraju <o180322@rguktong.ac.in>
2023-02-22 17:16:38 +0530

    files changed

---
+++ REST API/models/student.js
@@ -0,0 +1,40 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+
+
+
+
+//creating a geo location schema
+const GeoSchema = new Schema({
+    type:{
+        type:String,
+        default:"point"
+    },
+    coordinates:{
+        type:[Number],
+        index:"2dsphere"
+    }
+});
+
+//create a schema and model for student
+const studentSchema=new Schema({
+    name:{
+        type: String,
+        required: [true,'Name field is required']
+    },
+    rank:{
+        type:String
+    },
+    availabile:{
+        type:Boolean,
+        default:false
+    },
+   /* long:{}
+    lat:{}
+    */
+    geometry:GeoSchema
+});
+
+const Student = mongoose.model('student',studentSchema);
+module.exports=Student;
